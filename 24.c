c5f92cce542898c270f7012a3e43753d3fffe6e3
anbu <35687834+anbudhanush5@users.noreply.github.com>
2018-03-28 15:55:39 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[10];
+	scanf("%s",a);
+	int i,flag=0;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+			flag=1;
+		}
+		else
+		{
+			flag=0;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("yes");
+	}
+	else
+	printf("no");
+	getch();
+}
